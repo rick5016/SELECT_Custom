2a05dc844314448aff1f1313fe6dd954e6f4fa42
rick5016 <rick5016@gmail.com>
2019-03-04 23:11:30 +0100

    width auto

--- select.js
+++ select.js
@@ -4,25 +4,40 @@ jQuery(function () {
      * Gestion de la selection des select
      * Info : blur prend le pas sur click, mousedown passe avant le blur
      */
-    $(".select").on('mousedown', 'li', function () {
+    $(".select-list").on('mousedown', 'li', function () {
         var parentUL = $(this).parent('ul');
         var parentDIV = parentUL.parent('div');
         var selectID = parentUL.attr('class').split("select ")[1];
         parentDIV.prev('button').html($(this).html());
-        parentDIV.toggle();
+
+
+        //parentDIV.toggle();
+        var visibility = parentDIV.css('visibility');
+
+        if (visibility === 'hidden')
+        {
+            parentDIV.css('visibility', 'initial');
+            parentDIV.css('height', '80px');
+        } else {
+            parentDIV.css('visibility', 'hidden');
+            parentDIV.css('height', '0px');
+        }
+
+
         $("#" + selectID).val($(this).attr('value'));
         $("#" + selectID).change();
     });
 
     // Cache le select quand on clique en dehors
-    $(document.body).on('blur', '.select-head', function () {
-        $(this).next('div').hide();
+    $(document.body).on('blur', '.value-selected', function () {
+        $( this ).next('div').css('visibility', 'hidden');
+        $( this ).next('div').css('height', '0px');
     });
 
     // Recherche, déplacement puis sélection de l'élément ayant la premiere lettre de la touche pressée dans le select
-    $('.select-head').keypress(function (e) {
+    $('.value-selected').keypress(function (e) {
         var touche = String.fromCharCode(e.which);
-        var DIV = $(this).next('div'); //selectblock
+        var DIV = $(this).next('div'); //select-list-container
         var UL = DIV.children('ul'); //select
 
         // Recherche de l'element par rapport à la lettre
@@ -55,18 +70,28 @@ jQuery(function () {
     });
 
     // Select : Reset du CSS des éléments si on bouge la souris
-    $('.select-container').on('mousemove', 'ul', function () {
+    $('.select-bloc').on('mousemove', 'ul', function () {
         $(this).children('li').each(function () {
             $(this).removeClass('select-hover');
         });
     });
 
     // Select : Gestion du z-index de chaque select de la page
-    $(document.body).on('click', '.select-head', function () {
-        $(this).next('div').toggle();
+    $(document.body).on('click', '.value-selected', function () {
+        //$(this).next('div').toggle();
+        var visibility = $( this ).next('div').css('visibility');
+
+        if (visibility === 'hidden')
+        {
+            $( this ).next('div').css('visibility', 'initial');
+            $( this ).next('div').css('height', '80px');
+        } else {
+            $( this ).next('div').css('visibility', 'hidden');
+            $( this ).next('div').css('height', '0px');
+        }
 
         var zindex = 900;
-        $(".select-container").each(function () {
+        $(".select-bloc").each(function () {
             zindex = --zindex;
             $(this).css('z-index', zindex);
         });
